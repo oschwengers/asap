9378601f826e46d92b07c224c3e174274f2181f3
Oliver Schwengers <oliver.schwengers@computational.bio.uni-giessen.de>
2017-11-17 16:07:23 +0100

    add an admin database update script for CARD db

---
+++ scripts/admin/asap-update-card.sh
@@ -0,0 +1,23 @@
+
+curl -o card-software.tar.bz2 https://card.mcmaster.ca/latest/software
+tar -xjf card-software.tar.bz2
+tar -xzf release-rgi-*
+
+mv release-rgi-*/ card/
+rm -r card/_docs card/tests
+
+
+curl -o card-data.tar.bz2 https://card.mcmaster.ca/latest/data
+mkdir card-data
+tar -xjf card-data.tar.bz2 --directory card-data
+
+/usr/bin/env python card/load.py -i card-data/card.json
+/usr/bin/env python card/clean.py
+
+cp card-data/aro.json card
+rm -r *.bz2 *.gz card-data
+
+#DATA_VERSION=$(/usr/bin/env python card/rgi.py -dv)
+#RGI_VERSION=$(/usr/bin/env python card/rgi.py -sv)
+#echo "data version: ${DATA_VERSION}"
+#echo "rgi version: ${RGI_VERSION}"
