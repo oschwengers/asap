d45ccb5e01090c585557250ae6d32f05c971da61
Oliver Schwengers <oliver.schwengers@computational.bio.uni-giessen.de>
2018-12-03 18:27:10 +0100

    update abr admin scripts to card version 4.2.2 (RGI) and 3.0.0 (data)

--- scripts/admin/asap-update-card.sh
+++ scripts/admin/asap-update-card.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 
-curl -o card-software.tar.bz2 https://card.mcmaster.ca/latest/software
-tar -xjf card-software.tar.bz2
-tar -xzf release-rgi-*
-
-mv release-rgi-*/ card/
-rm -r card/_docs card/tests
-
-
 curl -o card-data.tar.bz2 https://card.mcmaster.ca/latest/data
 mkdir card-data
 tar -xjf card-data.tar.bz2 --directory card-data
+/usr/bin/env python3 $ASAP_HOME/share/card/rgi load -i card-data/card.json
+/usr/bin/env python3 $ASAP_HOME/share/card/rgi clean
 
-/usr/bin/env python3 card/load.py -i card-data/card.json
-/usr/bin/env python3 card/clean.py
+curl -o card-ontology.tar.bz2 https://card.mcmaster.ca/latest/ontology
+mkdir card-ontology
+tar -xjf card-ontology.tar.bz2 --directory card-ontology
+cp card-ontology/aro.json $ASAP_HOME/share/card/
+rm -r card-*
 
-cp card-data/aro.json card
-rm -r *.bz2 *.gz card-data
-
-#DATA_VERSION=$(/usr/bin/env python3 card/rgi.py -dv)
-#RGI_VERSION=$(/usr/bin/env python3 card/rgi.py -sv)
-#echo "data version: ${DATA_VERSION}"
-#echo "rgi version: ${RGI_VERSION}"
+DATA_VERSION=$(/usr/bin/env python3 $ASAP_HOME/share/card/rgi database)
+RGI_VERSION=$(/usr/bin/env python3 $ASAP_HOME/share/card/rgi main -v)
+echo "data version: ${DATA_VERSION}"
+echo "rgi version: ${RGI_VERSION}"
