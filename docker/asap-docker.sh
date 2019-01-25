407a3fa43066a77f5d9716ae3d298658c3a3a208
Oliver Schwengers <oliver.schwengers@computational.bio.uni-giessen.de>
2019-01-25 10:17:15 +0100

    add scratch dir option to asap-docker.sh script

--- docker/asap-docker.sh
+++ docker/asap-docker.sh
@@ -1,34 +1,51 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
-  echo 1>&2 "Usage: $0 <ASA³P_DIR> <PROJECT_DIR>"
-  exit 3
+if [ $# -ne 2 ] && [ $# -ne 3 ]; then
+    echo 1>&2 "Usage: $0 <ASA³P_DIR> <PROJECT_DIR> [<SCRATCH_DIR>]"
+    exit 3
 fi
 
 set -e
 
 if [ ! -d $1 ]
-  then
-	echo "ASA³P directory does not exist!"
-	exit 1
-  else
-	ASAP="$(dirname $(readlink -e $1))/$(basename $1)"
+    then
+        echo "ASA³P directory does not exist!"
+        exit 1
+    else
+        ASAP=$(realpath $1)
+        echo "ASA³P:   $ASAP"
 fi
 
 if [ ! -d $2 ]
-  then
-	echo "Project directory does not exist!"
-	exit 1
-  else
-	DATA="$(dirname $(readlink -e $2))/$(basename $2)"
+    then
+        echo "Project directory does not exist!"
+        exit 1
+    else
+        DATA=$(realpath $2)
+        echo "DATA:    $DATA"
 fi
 
-echo "ASA³P: $ASAP"
-echo "DATA:  $DATA"
-
-sudo docker run \
-	--rm \
-	-v $ASAP:/asap:ro \
-	-v $DATA:/data \
-	oschwengers/asap
-
+if [ $# -eq 3 ]
+    then
+        if [ ! -d $3 ]
+            then
+                echo "Scratch directory does not exist!"
+                exit 1
+            else
+                SCRATCH=$(realpath $3)
+                echo "Scratch: $SCRATCH"
+                sudo docker run \
+                    --rm \
+                    -v $ASAP:/asap:ro \
+                    -v $DATA:/data \
+                    -v $SCRATCH:/var/scratch \
+                    oschwengers/asap
+        fi
+    else
+        echo "Scratch: internal container scratch"
+	sudo docker run \
+            --rm \
+            -v $ASAP:/asap:ro \
+            -v $DATA:/data \
+            oschwengers/asap
+fi
