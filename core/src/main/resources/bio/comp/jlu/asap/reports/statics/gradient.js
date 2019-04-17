ea1c29db184ba79baa60a378e01594b7fd1146f2
oschwengers <oliver.schwengers@computational.bio.uni-giessen.de>
2019-04-17 09:13:01 +0200

    fix data table gradients in report pages

--- core/src/main/resources/bio/comp/jlu/asap/reports/statics/gradient.js
+++ core/src/main/resources/bio/comp/jlu/asap/reports/statics/gradient.js
@@ -1,28 +1,28 @@
 
-// get all gradients
-let nodes = document.querySelectorAll('[gradient]');
+// group elements by gradient id
 let dict = {};
-// fill dict
 Array.from( document.querySelectorAll('[gradient]') ).forEach( e => {
     const key = e.getAttribute( 'gradient' );
-    if( dict[key] !== undefined ) {
-        const list = Array.from( dict[key] );
+    if( key in dict ) {
+        let list = dict[key];
         list.push(e);
-        dict[key] = list;
     } else
         dict[key] = [e];
 } );
 
-// use dict
+// calculate visual properties of elements
 for (const [key, elem] of Object.entries(dict)) {
     let values = elem.map( (e, i, arr) => e.childNodes[0].textContent.replace( /\./g, '' ).replace( /,/g, '.' ) );
     const max = Math.max(...values.filter( it => it !== '-' ));
     for( let i = 0; i < elem.length; i++ ) {
-        let left = values[i] / max * 100; // percents of left
-        if( elem[i].getAttribute( 'zs-warning' ) === '' )
-            elem[i].style.backgroundImage = `-webkit-linear-gradient(left, #FFA5A7 ${left}%, #FFD1D2 ${left}%)`;
-        else
-            elem[i].style.backgroundImage = `-webkit-linear-gradient(left, #A2CDF1 ${left}%, transparent ${left}%)`;
-        elem[i].style.borderRight = '1px solid white';
+        let val = values[i];
+        if( val !== '-' ) {
+            let left = val / max * 100; // percents of left
+            if( elem[i].getAttribute( 'zs-warning' ) === '' )
+                elem[i].style.backgroundImage = `-webkit-linear-gradient(left, #FFA5A7 ${left}%, #FFD1D2 ${left}%)`;
+            else
+                elem[i].style.backgroundImage = `-webkit-linear-gradient(left, #A2CDF1 ${left}%, transparent ${left}%)`;
+            elem[i].style.borderRight = '1px solid white';
+        }
     }
 };
