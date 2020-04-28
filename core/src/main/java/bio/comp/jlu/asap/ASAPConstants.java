02acf1fae8d2e82e1d85809d890b202a00742329
oschwengers <oliver.schwengers@computational.bio.uni-giessen.de>
2020-04-28 15:45:39 +0200

    remove obsolete MAX_WAITING_PERIOD constant

--- core/src/main/java/bio/comp/jlu/asap/ASAPConstants.java
+++ core/src/main/java/bio/comp/jlu/asap/ASAPConstants.java
@@ -8,7 +8,6 @@ package bio.comp.jlu.asap;
  */
 public final class ASAPConstants {
 
-public final static int CONFIG_WRITER_SLEEP_PERIOD = 10000;
 public final static int MAX_STEP_WAITING_PERIOD = 24*60*60*1000;
 public final static int EXP_BACKOFF_EXP = 10;
 
